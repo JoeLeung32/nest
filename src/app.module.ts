a1d049a6a3cb6de7e1faed6bd15d2cf2482935aa
Joe Leung <joeleung32@yahoo.com.hk>
2022-08-04 11:04:33 +0800

    Generic exception handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { RouterModule, Routes } from '@nestjs/core';
+import { APP_FILTER, RouterModule, Routes } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule, ApiRoutes } from './api/api.module';
 import { CpModule, CPRoutes } from './cp/cp.module';
+import { AllExceptionFilter } from './filters/all-exception.filter';
 
 const routes: Routes = [
   {
@@ -21,6 +22,12 @@ const routes: Routes = [
 @Module({
   imports: [ApiModule, CpModule, RouterModule.register(routes)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
